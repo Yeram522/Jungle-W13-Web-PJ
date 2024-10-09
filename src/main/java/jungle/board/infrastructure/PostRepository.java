d53bb268f19f2bbde33620965b6666969f50a6df
yujin0124 <yujin0124@khu.ac.kr>
2024-10-09 23:34:37 +0900

    WIP: Implement initial comment functionality

--- src/main/java/jungle/board/infrastructure/PostRepository.java
+++ src/main/java/jungle/board/infrastructure/PostRepository.java
@@ -3,5 +3,7 @@ package jungle.board.infrastructure;
 import jungle.board.domain.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface PostRepository extends JpaRepository<Post, Long> {
 }
